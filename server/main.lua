6e40fa8cbfbe20fd116ed1283c487f95afe98400
Torch ICO <tommyrpuk@gmail.com>
2024-03-24 17:59:25 +0000

    Restarting payment system, gonna seek help from experts for SQL

--- server/main.lua
+++ server/main.lua
@@ -1,16 +1,11 @@
 Duty = false
-
 local function PaymentOffDuty(HasJob, Duty)
-    for i in Config.OffDutyPay do
-        for v in #HasJob do
-            HasJob = HasJob[v]
-        Payment = #Config.OffDutyPay[i]
-            while HasJob and not Duty do
-                exports.pefcl:addBankBalance(source, { amount = Payment, message = HasJob.name })
-                Wait(Config.PayoutPeriod * 10000)
-            end
-        end
-    end
+    local Player = Ox.GetPlayer(source)
+    local Groups = Player.getGroups()
+    for group, grade in pairs(Groups) do
+        table.sort(Groups)
+
+     end
 end
 
 local function Payment(HasJob, Duty)
@@ -18,16 +13,8 @@ local function Payment(HasJob, Duty)
     local Groups = Player.getGroups()
     for group, grade in pairs(Groups) do
         table.sort(Groups)
-        Work = {
-            group = group,
-            grade = grade
-        }
-        local payment = Config.OffDutyPay
-        while HasJob and Duty do
-            exports.pefcl:addBankBalance(source, { amount = payment, message = group.." "..grade })
-            Wait(Config.PayoutPeriod * 10000)
-        end
-    end
+
+     end
 end
 
 RegisterNetEvent('pu_duty:server:toggle', function()
@@ -35,6 +22,6 @@ RegisterNetEvent('pu_duty:server:toggle', function()
 end)
 
 RegisterNetEvent('pu_duty:server:paychecks', function(source, stateId)
-    local player = Ox.GetPlayer(source)
+    local Player = Ox.GetPlayer(source)
 end)
 
