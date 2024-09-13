97dfe7bef4d08a9b6126a8f889190a389ea86f77
ParuThePro <35810485+paruthepro@users.noreply.github.com>
2024-09-13 03:53:23 +0100

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,29 @@
+Config = {}
+
+Config.DutyLocations = {
+    [1] = { -- MRPD Front Desk
+        coords = vec3(441.82446289062, -982.05499267578, 30.807472229004),
+        rotation = 0,
+        job = {['police'] = 0, ['bcso'] = 0, ['srt'] = 0, ['doc'] = 0}
+    },
+    [2] = { -- Sandy PD Front Desk
+        coords = vec3(1833.7335205078, 3678.4045410156, 34.217071533203),
+        rotation = 0,
+        job = {['police'] = 0, ['bcso'] = 0, ['srt'] = 0, ['doc'] = 0}
+    },
+    [3] = { -- Paleto PD Front Desk
+        coords = vec3(-447.27453613281, 6013.033203125, 32.406543731689),
+        rotation = 0,
+        job = {['police'] = 0, ['bcso'] = 0, ['srt'] = 0, ['doc'] = 0}
+    },
+    [4] = { --Davis PD Desk Left of Front Door
+        coords = vec3(382.03057861328, -1596.384765625, 29.9130859375),
+        rotation = 0,
+        job = {['police'] = 0, ['bcso'] = 0, ['srt'] = 0, ['doc'] = 0}
+    },
+    [5] = { -- MBA Stadium Keypad
+        coords = vec3(-281.73825073242, -1937.3631591797, 30.474990844727),
+        rotation = 0,
+        job = {['mba'] = 0}
+    }
+}
